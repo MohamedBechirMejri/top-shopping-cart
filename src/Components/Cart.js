bec76f03794bfa4baf2f16ec2354df932109cda6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-05 16:26:01 +0100

    make cart responsive

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -20,29 +20,29 @@ const Cart = (props) => {
     <>
       <div
         ref={ref}
-        className={`border-l-2 h-screen w-[30vw] fixed z-10 transition-all ease-in-out duration-[250ms]  border-black bg-white flex flex-col ${
-          props.isOpen ? "right-0" : "-right-[30vw]"
+        className={`border-l-2 h-screen w-[85vw] sm:w-[30vw] fixed z-10 transition-all ease-in-out duration-[250ms]  border-black bg-white flex flex-col ${
+          props.isOpen ? "right-0" : "-right-[100vw] sm:-right-[35vw]"
         } items-center justify-start gap-2 p-4  ${
           props.isOpen ? " ring-[#00000070]" : "ring-[#00000000] "
         } ring-[100vw] `}
       >
-        <h1 className="uppercase font-extrabold text-2xl m-4">your Cart</h1>
-        <div className="w-full flex flex-col items-start justify-start gap-4  ">
+        <h1 className="m-4 text-2xl font-extrabold uppercase">your Cart</h1>
+        <div className="flex flex-col items-start justify-start w-full gap-4 overflow-scroll">
           {items.map((item) => (
             <div
-              className="flex border border-black  items-center justify-start w-full text-center "
+              className="flex items-center justify-start w-full text-center border border-black "
               key={item.id}
             >
               <img src={item.image} alt={item.name} className="w-32 h-32" />
-              <div className="grow gap-2 flex flex-col">
-                <p className="text-center text-xl font-semibold">{item.name}</p>
-                <p className="text-center text-xl">${item.price}</p>
+              <div className="flex flex-col gap-2 grow">
+                <p className="text-xl font-semibold text-center">{item.name}</p>
+                <p className="text-xl text-center">${item.price}</p>
                 <div className="flex items-center justify-center gap-4">
-                  <button className="bg-black text-white font-medium   justify-center items-center flex  px-2">
+                  <button className="flex items-center justify-center px-2 font-medium text-white bg-black">
                     -
                   </button>
-                  <p className="text-center text-xl">{item.quantity}</p>
-                  <button className="bg-black text-white font-medium   justify-center items-center flex px-2">
+                  <p className="text-xl text-center">{item.quantity}</p>
+                  <button className="flex items-center justify-center px-2 font-medium text-white bg-black">
                     +
                   </button>
                 </div>{" "}
@@ -52,7 +52,7 @@ const Cart = (props) => {
         </div>
         <hr />
         <p className="text-xl font-bold">Total: ${total}</p>
-        <button className="bg-black text-white p-2 px-12 font-medium uppercase text-xl">
+        <button className="p-2 px-12 text-xl font-medium text-white uppercase bg-black">
           Checkout
         </button>
       </div>
