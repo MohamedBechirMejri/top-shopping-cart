345611503918f3d5282a2e74078a16a28fcfcc96
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-06 02:40:54 +0100

    add transition

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="py-[5em] flex items-center justify-center flex-col h-screen text-2xl font-extrabold w-screen ">
       <p>BEST ONLINE STORE OF THE YEAR</p>
       <Link to="shop">
-        <button className="p-4 text-2xl font-extrabold text-white uppercase bg-black ">
+        <button className="p-4 text-2xl font-extrabold text-white uppercase transition bg-black hover:scale-105">
           start shopping!
         </button>
       </Link>
